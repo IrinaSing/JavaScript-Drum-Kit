d6b70ed7139323f5b57bd90bd3e1d83dac41bd2a
IrinaSing <irinasingh.mail@yandex.ru>
2021-06-08 22:31:22 +0200

    playsound handler

---
+++ src/handlers/playsound.js
@@ -0,0 +1,18 @@
+/* import { data } from "../data.js";
+import { isWord } from "../logic/is-word.js";
+import { sortStrings } from "../logic/sort-strings.js";
+import { updateList } from "../procedures/update-list.js";
+*/
+
+/**
+ * @param {Event} e - The event triggered when the user clicks the button.
+ */
+
+export const playSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop the function from running all together
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  key.classList.add("playing");
+};
