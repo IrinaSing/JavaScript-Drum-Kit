450b7f95d8e6de8719fd90f1f4c55f16e051eb01
IrinaSing <irinasingh.mail@yandex.ru>
2021-06-09 10:34:55 +0200

    +

---
+++ src/handlers/remove-transition.js
@@ -0,0 +1,15 @@
+/* import { data } from "../data.js";
+import { isWord } from "../logic/is-word.js";
+import { sortStrings } from "../logic/sort-strings.js";
+import { updateList } from "../procedures/update-list.js";
+*/
+
+/**
+ * @param {Event} e - The event triggered after button's effects were played.
+ */
+
+export const removeTransition = (e) => {
+  if (e.propertyName !== "transform") return; // skip it if it's not a transform
+  this.classList.remove("playing");
+  console.log(e.propertyName);
+};
---
+++ src/init/index.js
@@ -0,0 +1,13 @@
+import { data } from "../data.js";
+//import { updateList } from "../procedures/update-list.js";
+
+/* --- import all listener files so they can attach to the DOM --- */
+
+// eslint-disable-next-line sort-imports
+import "../listeners/play-sound.js";
+import "../listeners/remove-transition.js";
+
+/* --- any other code that executes when the document or window are ready --- */
+
+// render the initial list
+//updateList(data.words);
